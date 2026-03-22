6cfcccb0fab36be061c9a10deafa480279caddd0
premananda <djoty108@gmail.com>
2026-03-22 15:16:27 +0200

    Формулы

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,17 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://ether-blog.vercel.app',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	},
 	i18n: {
 		defaultLocale: 'en',
 		locales: ['en', 'ru'],
